d1b9914866281e7c8dab0bcebcfc57ed8249ba72
try4l <alan.b.davison@gmail.com>
2018-01-22 16:54:31 -0800

    Added NumList and SimpleForm components. Added example reactstrap form to the Exp component page.

--- src/components/NumList/NumList.js
+++ src/components/NumList/NumList.js
@@ -6,16 +6,28 @@ function ListItem(props) {
   return <li>{props.value}</li>;
 }
 
+// function NumberList(props) {
+//   const numbers = props.numbers;
+//   const listItems = numbers.map((number) =>
+//     // Correct! Key should be specified inside the array.
+//     <ListItem key={number.toString()}
+//               value={number} />
+//   );
+//   return (
+//     <ul>
+//       {listItems}
+//     </ul>
+//   );
+// }
+
 function NumberList(props) {
   const numbers = props.numbers;
-  const listItems = numbers.map((number) =>
-    // Correct! Key should be specified inside the array.
-    <ListItem key={number.toString()}
-              value={number} />
-  );
   return (
     <ul>
-      {listItems}
+      {numbers.map((number) =>
+        <ListItem key={number.toString()}
+                  value={number} />
+      )}
     </ul>
   );
 }
---
+++ src/components/SimpleForm/SimpleForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+
+class SimpleForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value1: 'me', value2: '0'};
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+      const target = event.target;
+      const name = target.name;
+      const value = target.value;
+        this.setState({
+      [name]: value
+    });
+  }
+  
+  
+
+  handleSubmit(event) {
+    alert('A name and address was submitted: ' + this.state.value1 + "  " + this.state.value2);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input name="value1" type="text" value={this.state.value1} onChange={this.handleChange} />
+        </label>
+       <label>
+          Address:
+          <input name="value2" type="text" value={this.state.value2} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default SimpleForm;
