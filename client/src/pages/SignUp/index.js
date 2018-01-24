bd4128d838ecc9c52157b6aed60786fc9c05784e
try4l <alan.b.davison@gmail.com>
2018-01-23 16:12:24 -0800

    Added client/.gitignore and trucklist-controller.js files. Made SignUp landing page which redirects to either the Admin User page or the Client User page.

--- client/src/pages/SignUp/index.js
+++ client/src/pages/SignUp/index.js
@@ -2,23 +2,68 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 
+import { Form, FormGroup, Label, Input } from 'reactstrap';
+import { Card, Button, ButtonGroup, CardTitle, CardText, Row, Col, CardLink } from 'reactstrap';
+
 import './style.css';
 
 class SignUp extends Component {
-  static propTypes = {}
-  static defaultProps = {}
-  state = {}
+  constructor (props) {
+    super(props);
+
+    this.state = { rSelected: 1};
+
+    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
+  }
+
+  onRadioBtnClick(rSelected) {
+    this.setState({ rSelected });
+  }
+
+  // When the component mounts, show clicked button
+  componentDidMount() {
+    //this.state.rSelected=1;
+  }
 
   render() {
     const { className, ...props } = this.props;
     return (
       <div className={classnames('About', className)} {...props}>
-        <h1>
-          Sign Up
-        </h1>
+        <Form>
+
+          <FormGroup tag="fieldset">
+            <legend>User Type</legend>
+            <FormGroup check inline>
+              <Label check>
+                <Input type="radio" name="radio1" checked={this.state.rSelected===1} onClick={() => this.onRadioBtnClick(1)} active={this.state.rSelected === 1} />{' '}
+                Foodie
+              </Label>
+            </FormGroup>
+            <FormGroup check inline>
+              <Label check>
+                <Input type="radio" name="radio1" onClick={() => this.onRadioBtnClick(2)} active={this.state.rSelected === 2} />{' '}
+                ADMIN
+              </Label>
+            </FormGroup>
+          </FormGroup>
+
+          <div className="row justify-content-center">
+            <Col sm="6">
+              <Card body>
+                <CardTitle>Tell us above.</CardTitle>
+                <CardText>Are you a site administrator or are you just hungry?</CardText>
+                <Button href={this.state.rSelected!==1? "/admin":" /user"} >Show Me! </Button>
+              </Card>
+            </Col>
+          </div>
+
+          <p>Selected: {this.state.rSelected===1? "/user":" /admin"}</p>
+
+        </Form>
       </div>
     );
   }
 }
+
 
 export default SignUp;
